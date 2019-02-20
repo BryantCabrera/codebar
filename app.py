39cf465d7f8cdb59658e0aab6a54c5c6f5ef5ad7
Bryant <cabrera.bryant@gmail.com>
2019-02-20 15:24:21 -0800

    Creates Workshop class.

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ bryant.introduce()
 class Student(Member):
     def __init__(self, full_name="", reason=""):
         Member.__init__(self, full_name)
+        self.account = 'student'
         self.reason = reason
 
 michael = Student("Michael", "I need to learn!")
@@ -22,6 +23,7 @@ michael.introduce()
 class Instructor(Member):
     def __init__(self, full_name="", bio="", skills=[]):
         Member.__init__(self, full_name)
+        self.account = 'instructor'
         self.bio = bio
         self.skills = skills
 
@@ -32,3 +34,40 @@ class Instructor(Member):
 peter = Instructor("Peter", "I teach d3", ['d3', 'topomaps'])
 peter.introduce()
 peter.add_skill('svg')
+
+class Workshop():
+    def __init__(self, date="", subject="", instructors=[], students=[]):
+        self.date = date
+        self.subject = subject
+        self.instructors = instructors
+        self.students = students
+
+    def add_participant(self, person):
+        if person.account == 'student':
+            self.students.append(person)
+        elif person.account == 'instructor':
+            self.instructors.append(person)
+
+    def print_details(self):
+        print(f"Workshop - {self.date} - {self.subject}\nStudents")
+        for index, student in enumerate(self.students):
+            print(f"{index + 1}. {student.full_name}")
+        print('\n Instructors')
+        for index, instructor in enumerate(self.instructors):
+            print(f"{index + 1}. {instructor.full_name}")
+
+workshop = Workshop("12/03/2014", "Shutl")
+
+jane = Student("Jane Doe", "I am trying to learn programming and need some help")
+lena = Student("Lena Smith", "I am really excited about learning to program!")
+vicky = Instructor("Vicky Python", "I want to help people learn coding.")
+vicky.add_skill("HTML")
+vicky.add_skill("JavaScript")
+nicole = Instructor("Nicole McMillan", "I have been programming for 5 years in Python and want to spread the love")
+nicole.add_skill("Python")
+
+workshop.add_participant(jane)
+workshop.add_participant(lena)
+workshop.add_participant(vicky)
+workshop.add_participant(nicole)
+workshop.print_details()
